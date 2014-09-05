dd0f4ccdb77c59de7066c377501677f48f911353
Alexander Diaz Chub <roboterhund@gmail.com>
2014-09-05 02:00:24 +0200

    added SCSS

---
+++ config.rb
@@ -0,0 +1,17 @@
+http_path = "/"
+
+css_dir = "static/css"
+sass_dir = "scss"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+
+# output_style = :nested
+output_style = :expanded
+# output_style = :compact
+# output_style = :compressed
+
+relative_assets = false
+
+line_comments = true
+
+
